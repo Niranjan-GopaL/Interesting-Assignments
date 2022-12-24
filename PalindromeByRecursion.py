3e1bab6b8a78d5388c1f6c6b134b462412211b67
Niranjan Gopal <120457695+Niranjan-GopaL@users.noreply.github.com>
2022-12-24 21:07:16 +0530

    Update PalindromeByRecursion.py

--- PalindromeByRecursion.py
+++ PalindromeByRecursion.py
@@ -6,7 +6,7 @@ def isPalindrome(n):
   elif n<10:
     return True
   else:
-    big10 = 10**(len(str(n)) - 1)                        # in n is 3 digit number , big10 is 100 ; if a 4 digit number then 
+    big10 = 10**(len(str(n)) - 1)                        # in n is 3 digit number , big10 is 100 ; if a 4 digit number then 1000
     newN = n - ((n//big10)*big10)
     newN = newN //10                                     #if n is 12321 , newN is 232  ; if n is 232 , newN is 3 and so on
     return (n%10 == n//(big10)) and isPalindrome(newN)
