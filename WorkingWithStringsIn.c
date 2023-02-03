289b8038dfba5534988baee9fa926e5e47fc8084
Niranjan Gopal <120457695+Niranjan-GopaL@users.noreply.github.com>
2023-02-04 02:15:41 +0530

    Update WorkingWithStringsIn.c

--- WorkingWithStringsIn.c
+++ WorkingWithStringsIn.c
@@ -2,35 +2,74 @@
 #include <stdlib.h>
 #include <string.h>
 
-int read_strings(char *a[], int n){
+#define MAX_STRING_LEN 20
+#define MAX_STRING 10
 
-}
 
-void swap(int a[], int i, int j){
-	int t = a[i];
-	a[i]=a[j];
-	a[j]=t;
-}
-
-void swap_greaterup(int a[], int i){
-	if (a[i] > a[i+1] )
-		swap(a,i,i+1);
-}
-
-void repeat_swap_greaterup(int *a[], int n){
-	for(int i=0;i<n-1;i++)
-		swap_greaterup(a,i);
-}
-
-void sort_strings(char *a[], int n)
-// By bubble sort
+int read_strings(char *a[], int _n)
 {
-	for(int i=0; i < n-1 ; i++)
-	    repeat_swap_greaterup(a,n); // do the repeated swap: (n-i, maybe?)
+  char buffer[MAX_STRING_LEN + 1];
+  int count = 0;
+  while (count < _n && scanf("%20s", buffer) == 1 && strcmp(buffer, "finish")) 
+  {
+    a[count] = (char*)malloc(strlen(buffer) + 1);
+    strcpy(a[count], buffer);
+    count++;
+  }
+  return count;
+}
+
+void sort_strings(char *a[], int _n) 
+{
+  for (int i = 0; i < _n - 1; i++) 
+  {
+    for (int j = i + 1; j < _n; j++) 
+    {
+      if (strcmp(a[i], a[j]) > 0) 
+      {
+        char *temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+    }
+  }
+}
+
+int main() 
+{
+  char *strings[MAX_STRING];
+  int count = read_strings(strings, MAX_STRING );
+  printf("\n");
+
+  for (int i = 0; i < count; i++) 
+    printf("%s\n", strings[i]);
+  
+  printf("\n");
+
+  sort_strings(strings, count);
+  for (int i = 0; i < count; i++) 
+    printf("%s\n", strings[i]);
+
+  return 0;
 }
 
 
-
-void main(){
-    
-}
+/*
+SAMPLE SESSION:
+Jack and Jill    went up    the    hill.   finish // This is the input (it will have the "finish" word at the end. 
+Jack       // This is the print of the input
+and
+Jill
+went
+up
+the
+hill.
+          // This is the extra line
+Jack      // This is printing the strings but after sorting.
+Jill
+and
+hill.
+the
+up
+went
+*/
