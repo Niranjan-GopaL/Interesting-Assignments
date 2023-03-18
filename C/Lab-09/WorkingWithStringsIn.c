7604dbb66a2f5590306a150aba6ca8a363d63cfc
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-03-18 21:24:47 +0530

    Added endsems

--- C/Lab-09/WorkingWithStringsIn.c
+++ C/Lab-09/WorkingWithStringsIn.c
@@ -3,17 +3,16 @@
 #include <string.h>
 
 #define MAX_STRING_LEN 20
-#define MAX_LINE 10
+#define MAX_IN_LINE 10
 
-
+//_n is MAX_IN_LINE 
 int read_strings(char *a[], int _n)
 {
   char buffer[MAX_STRING_LEN + 1];//the array to store each word
   int count = 0;
   // scanf("20s") means that it'll read at most 20 characaters which is the maximum len on string
   // since buffer is a character array scanfing or strcpying buffer is eqvt to &buffer[0]
-  while (count < _n && scanf("%20s", buffer) == 1 && strcmp(buffer, "finish")) 
-  {
+  while (count < _n && scanf("%20s", buffer) == 1 && strcmp(buffer, "finish")) {
     a[count] = (char*)malloc(strlen(buffer) + 1);
     strcpy(a[count++], buffer);
   }
@@ -38,8 +37,8 @@ void sort_strings(char *a[], int _n)
 
 int main() 
 {
-  char *strings[MAX_LINE];
-  int count = read_strings(strings, MAX_LINE );
+  char *strings[MAX_IN_LINE];
+  int count = read_strings(strings, MAX_IN_LINE );
   printf("\n");
 
   for (int i = 0; i < count; i++) 
